5cb4727cc50e784449e24712ea8d5eb28ab7be47
pavl_g <scrappers.tm@gmail.com>
2023-09-28 06:46:21 +0300

    JniUtils.h: utilized the thread-local JNIEnv pointers

--- serial4j-native/src/include/JniUtils.h
+++ serial4j-native/src/include/JniUtils.h
@@ -101,7 +101,7 @@ namespace JniUtils {
         jvmArgs->group = threadGroupObj;
         return jvmArgs;
     }
-    
+
     static inline jint attachNativeThread(char* threadName, jobject threadGroupObj) {
         void** jniEnvPointer = (void**) JniUtils::getJniEnv();
         struct JavaVMAttachArgs* jvmArgs = getJVMAttachArgs(JniUtils::version, threadName, threadGroupObj);
@@ -111,222 +111,223 @@ namespace JniUtils {
     /**
      * @brief Retrieves a java class from a name, the name is referred to by the
      * package/class, example: {"com/java/io/IOException"}.
-     * 
+     *
      * @param clazzName the class name using the format {package/class}.
      * @return jclass the reflected java class.
      */
-    static inline jclass getClassFromString(const char* clazzName) {
-        return (*JniUtils::getJniEnv())->FindClass(clazzName);
+    static inline jclass getClassFromString(JNIEnv* env, const char* clazzName) {
+        return env->FindClass(clazzName);
     }
 
     /**
      * @brief Retrieves a java class method using the [methodName] for the method name
      * and the [sig] for the method signature.
-     * 
+     *
      * @param clazzName the class name in the format {"package/class"}.
      * @param methodName the method name, use `<init>` for constructors.
      * @param sig the method signature, eg: ()V for non-parameterized void method.
      * @return jmethodID a new method id for the requested method.
      */
-    static inline jmethodID getClassMethod(const char* clazzName, const char* methodName, const char* sig) {
-        return (*JniUtils::getJniEnv())->GetMethodID(getClassFromString(clazzName), methodName, sig);
+    static inline jmethodID getClassMethod(JNIEnv* env, const char* clazzName, const char* methodName, const char* sig) {
+        return env->GetMethodID(getClassFromString(env, clazzName), methodName, sig);
     }
 
     /**
      * @brief Retrieves the java class constructor.
-     * 
+     *
      * @version version 0 from getClassConstructor for parameteized constructors using constructor
      * signature.
      * @param clazzName the java class name in the format {"package/class"}.
      * @param sig the constructor signature, eg: ()V for non-parameterized void type.
      * @return jmethodID an id referring to the class constructor.
      */
-    static inline jmethodID getClassConstructor0(const char* clazzName, const char* sig) {
-        return JniUtils::getClassMethod(clazzName, CONSTRUCTOR, sig);
+    static inline jmethodID getClassConstructor0(JNIEnv* env, const char* clazzName, const char* sig) {
+        return JniUtils::getClassMethod(env, clazzName, CONSTRUCTOR, sig);
     }
 
     /**
      * @brief Retrieves the java class non-parameterized constructor with signature
      * [NON_PARAMTERIZED_VOID_SIG].
-     * 
+     *
      * @param clazzName the java class name.
      * @return jmethodID an id referring to the class non-parameterized constructor.
      */
-    static inline jmethodID getClassConstructor1(const char* clazzName) {
-        return JniUtils::getClassConstructor0(clazzName, NON_PARAMTERIZED_VOID_SIG);
+    static inline jmethodID getClassConstructor1(JNIEnv* env, const char* clazzName) {
+        return JniUtils::getClassConstructor0(env, clazzName, NON_PARAMTERIZED_VOID_SIG);
     }
 
     /**
      * @brief Calls a parameterized method [methodName] from class [clazzName].
-     * 
+     *
      * @param clazzName the class name where the method is located, eg: {"java/io/IOException"}.
      * @param methodName the method name to call.
      * @param sig the signature of the method, eg: ()V for non-paramterized void methods.
      * @param args an array for the method parameters of type [jvalue] referring to one of the JNI
-     * data types [jint, jbyte, jlong, jshort, jchar, jobject...etc].
+     * data types [jint, jbyte, jlong, jshort, jchar, jweak...etc].
      */
-    static inline void callParameterizedMethod(const char* clazzName, const char* methodName, const char* sig, jvalue* args) {
-        jclass clazz = JniUtils::getClassFromString(clazzName);
-        jmethodID mid = JniUtils::getClassMethod(clazzName, methodName, sig);
+    static inline void callParameterizedMethod(JNIEnv* env, const char* clazzName, const char* methodName, const char* sig, jvalue* args) {
+        jclass clazz = JniUtils::getClassFromString(env, clazzName);
+        jmethodID mid = JniUtils::getClassMethod(env, clazzName, methodName, sig);
         if (args == NULL) {
-            (*JniUtils::getJniEnv())->CallVoidMethod(clazz, mid);
+            env->CallVoidMethod(clazz, mid);
         } else {
-            (*JniUtils::getJniEnv())->CallVoidMethod(clazz, mid, args);
+            env->CallVoidMethod(clazz, mid, args);
         }
     }
 
     /**
      * @brief Calls a non-parameterized method [methodName] with signature [sig].
-     * 
+     *
      * @param clazzName the class name where the method is located, eg: {"java/lang/String"}.
      * @param methodName the method name to call.
      * @param sig the signature of the method, eg: ()V, ()I, ()J.
      */
-    static inline void callNonParameterizedMethod(const char* clazzName, const char* methodName, const char* sig) {
-        JniUtils::callParameterizedMethod(clazzName, methodName, sig, NULL);
+    static inline void callNonParameterizedMethod(JNIEnv* env, const char* clazzName, const char* methodName, const char* sig) {
+        JniUtils::callParameterizedMethod(env, clazzName, methodName, sig, NULL);
     }
 
     /**
      * @brief Calls a class parameterized constructor (<init> method) with
      * signature and jvalue (holds only one variable of one JNI data type at a time) parameters.
-     * 
-     * @param clazzName 
-     * @param sig 
-     * @param args 
+     *
+     * @param clazzName
+     * @param sig
+     * @param args
      */
-    static inline void callParameterizedConstructor(const char* clazzName, const char* sig, jvalue* args) {
-        JniUtils::callParameterizedMethod(clazzName, CONSTRUCTOR, sig, args);
+    static inline void callParameterizedConstructor(JNIEnv* env, const char* clazzName, const char* sig, jvalue* args) {
+        JniUtils::callParameterizedMethod(env, clazzName, CONSTRUCTOR, sig, args);
     }
 
-    static inline void callNonParameterizedConstructor(const char* clazzName) {
-        JniUtils::callParameterizedConstructor(clazzName, NON_PARAMTERIZED_VOID_SIG, NULL);
+    static inline void callNonParameterizedConstructor(JNIEnv* env, const char* clazzName) {
+        JniUtils::callParameterizedConstructor(env, clazzName, NON_PARAMTERIZED_VOID_SIG, NULL);
     }
 
-    static inline jobject getObjectFromClass0(const char* clazzName) {
-        return (*JniUtils::getJniEnv())->AllocObject(JniUtils::getClassFromString(clazzName));
+    static inline jweak getObjectFromClass0(JNIEnv* env, const char* clazzName) {
+        return env->AllocObject(JniUtils::getClassFromString(env, clazzName));
     }
 
-    static inline jobject getObjectFromClass1(const char* clazzName, jmethodID constructor) {
-        return (*JniUtils::getJniEnv())->NewObject(JniUtils::getClassFromString(clazzName), constructor);
+    static inline jweak getObjectFromClass1(JNIEnv* env, const char* clazzName, jmethodID constructor) {
+        return env->NewObject(JniUtils::getClassFromString(env, clazzName), constructor);
     }
 
-    static inline jobject getObjectFromClass2(const char* clazzName) {
-        return JniUtils::getObjectFromClass1(clazzName, JniUtils::getClassConstructor1(clazzName));
+    static inline jweak getObjectFromClass2(JNIEnv* env, const char* clazzName) {
+        return JniUtils::getObjectFromClass1(env, clazzName, JniUtils::getClassConstructor1(env, clazzName));
     }
 
-    static inline jclass getObjectClass(jobject* object) {
-        return (*JniUtils::getJniEnv())->GetObjectClass(*object);
+    static inline jclass getObjectClass(JNIEnv* env, jweak* object) {
+        return env->GetObjectClass(*object);
     }
 
-    static inline jfieldID getFieldIDFromObject(jobject* object, const char* fieldName, const char* fieldSig) {
-        return (*JniUtils::getJniEnv())->GetFieldID(getObjectClass(object), fieldName, fieldSig);
+    static inline jfieldID getFieldIDFromObject(JNIEnv* env, jweak* object, const char* fieldName, const char* fieldSig) {
+        return env->GetFieldID(getObjectClass(env, object), fieldName, fieldSig);
     }
 
-    static inline jobject getObjectFieldFromClass(jobject* object, const char* fieldName, const char* fieldSig) {
-        jfieldID fieldId = getFieldIDFromObject(object, fieldName, fieldSig);
-        return (*JniUtils::getJniEnv())->GetObjectField(*object, fieldId);
+    static inline jweak getObjectFieldFromClass(JNIEnv* env, jweak* object, const char* fieldName, const char* fieldSig) {
+        jfieldID fieldId = getFieldIDFromObject(env, object, fieldName, fieldSig);
+        return env->GetObjectField(*object, fieldId);
     }
 
-    static inline jint getIntFieldFromClass(jobject* object, const char* fieldName, const char* fieldSig) {
-        jfieldID fieldId = getFieldIDFromObject(object, fieldName, fieldSig);
-        return (*JniUtils::getJniEnv())->GetIntField(*object, fieldId);
+    static inline jint getIntFieldFromClass(JNIEnv* env, jweak* object, const char* fieldName, const char* fieldSig) {
+        jfieldID fieldId = getFieldIDFromObject(env, object, fieldName, fieldSig);
+        return env->GetIntField(*object, fieldId);
     }
 
-    static inline void setObjectField(jobject* object, const char* fieldName, const char* fieldSig, jobject value) {
-        jfieldID fieldId = getFieldIDFromObject(object, fieldName, fieldSig);
-        (*JniUtils::getJniEnv())->SetObjectField(*object, fieldId, value);
+    static inline void setObjectField(JNIEnv* env, jweak* object, const char* fieldName, const char* fieldSig, jweak value) {
+        jfieldID fieldId = getFieldIDFromObject(env, object, fieldName, fieldSig);
+        env->SetObjectField(*object, fieldId, value);
     }
 
-    static inline void setIntField(jobject* object, const char* fieldName, const char* fieldSig, jint value) {
-        jfieldID fieldId = getFieldIDFromObject(object, fieldName, fieldSig);
-        (*JniUtils::getJniEnv())->SetIntField(*object, fieldId, value);
+    static inline void setIntField(JNIEnv* env, jweak* object, const char* fieldName, const char* fieldSig, jint value) {
+        jfieldID fieldId = getFieldIDFromObject(env, object, fieldName, fieldSig);
+        env->SetIntField(*object, fieldId, value);
     }
 
     /**
      * Translates a java TerminalDevice object into a native TerminalDevice object.
-     * 
+     *
      * @param object a pointer referring to the java TerminalDevice object.
      * @return a native terminal device holding the jdk terminal device's data.
      */
-    static inline jobject getSerialPortFromTerminalDevice(jobject* object) {
-        return getObjectFieldFromClass(object, "serialPort", "Lcom/serial4j/core/serial/SerialPort;");
+    static inline jweak getSerialPortFromTerminalDevice(JNIEnv* env, jweak* object) {
+        return getObjectFieldFromClass(env, object, "serialPort", "Lcom/serial4j/core/serial/SerialPort;");
     }
 
-    static inline int getPortDescriptorFromSerialPort(jobject* object) {
-        jobject serialPortObject = getSerialPortFromTerminalDevice(object);
-        return getIntFieldFromClass(&serialPortObject, "fd", "I");
+    static inline int getPortDescriptorFromSerialPort(JNIEnv* env, jweak* object) {
+        jweak serialPortObject = getSerialPortFromTerminalDevice(env, object);
+        return getIntFieldFromClass(env, &serialPortObject, "fd", "I");
     }
 
-    static inline jobjectArray createNewArrayFromBuffer(const char* clazzName, jsize length) {
-        jobject initialElement = (*JniUtils::getJniEnv())->NewStringUTF("");
+    static inline jobjectArray createNewArrayFromBuffer(JNIEnv* env, const char* clazzName, jsize length) {
+        jstring initialElement = env->NewStringUTF("");
 
-        jclass clazz = JniUtils::getClassFromString(clazzName);
-        jobjectArray array = (*JniUtils::getJniEnv())->NewObjectArray(length, clazz, initialElement);
-        return array;
+        jclass clazz = JniUtils::getClassFromString(env, clazzName);
+
+        return env->NewObjectArray(length, clazz, initialElement);
     }
 
-    static inline jobject* getObjectBufferFromString(const char** buffer, int length) {
-        jobject* objectBuffer = (jobject*) calloc(length, sizeof(jobject));
+    static inline const char* getBufferFromString(JNIEnv* env, jstring string) {
+        const char* chars = env->GetStringUTFChars(string, 0);
+        return chars;
+    }
+
+    static inline jstring getStringFromBuffer(JNIEnv* env, const char* buffer) {
+        return env->NewStringUTF(buffer);
+    }
+
+    static inline jobjectArray getStringArrayFromBuffer(JNIEnv* env, const char** buffer, int length) {
+        jobjectArray objectBuffer = JniUtils::createNewArrayFromBuffer(env, java_lang_String, length);
         for (int i = 0; i < length; i++) {
-            objectBuffer[i] = (*JniUtils::getJniEnv())->NewStringUTF(buffer[i]);
+            env->SetObjectArrayElement(objectBuffer, i, getStringFromBuffer(env, buffer[i]));
         }
         return objectBuffer;
-    } 
-
-    static inline const char* getBufferFromString(jstring string) {
-        return (*JniUtils::getJniEnv())->GetStringUTFChars(string, 0);
     }
 
-    static inline jstring getStringFromBuffer(const char* buffer) {
-        return (*JniUtils::getJniEnv())->NewStringUTF(buffer);
-    }
-
-    static inline void setArrayElements(jobjectArray array, jsize length, jobject* buffer) {
+    static inline void setArrayElements(JNIEnv* env, jobjectArray array, jsize length, jobject* values) {
         for (int i = 0; i < length; i++) {
-            (*JniUtils::getJniEnv())->SetObjectArrayElement(array, i, buffer[i]);
+            env->SetObjectArrayElement(array, i, values[i]);
         }
     }
 
-    static inline jobject getArrayElement(jobjectArray* array, jsize index) {
-        return (*JniUtils::getJniEnv())->GetObjectArrayElement(*array, index);
-    } 
-
-    static inline jint getIntArrayElement(jintArray* array, jsize index) {
-        return (*JniUtils::getJniEnv())->GetIntArrayElements(*array, 0)[index];
+    static inline jweak getArrayElement(JNIEnv* env, jobjectArray* array, jsize index) {
+        return env->GetObjectArrayElement(*array, index);
     }
 
-    static inline jbyte* getByteArrayElement(jbyteArray* array, jsize index) {
-        return (*JniUtils::getJniEnv())->GetByteArrayElements(*array, 0);
+    static inline jint getIntArrayElement(JNIEnv* env, jintArray* array, jsize index) {
+        return env->GetIntArrayElements(*array, 0)[index];
     }
 
-    static inline jintArray getIntArrayFromBuffer(int* buffer, int length) {
-        jintArray intArray = (*JniUtils::getJniEnv())->NewIntArray(length);
-        (*JniUtils::getJniEnv())->SetIntArrayRegion(intArray, 0, length, buffer);
+    static inline jbyte* getByteArrayElement(JNIEnv* env, jbyteArray* array, jsize index) {
+        return env->GetByteArrayElements(*array, 0);
+    }
+
+    static inline jintArray getIntArrayFromBuffer(JNIEnv* env, int* buffer, int length) {
+        jintArray intArray = env->NewIntArray(length);
+        env->SetIntArrayRegion(intArray, 0, length, buffer);
         return intArray;
     }
 
-    static inline jbyteArray getByteArrayFromBuffer(signed char* buffer, int length) {
-        jbyteArray byetArray = (*JniUtils::getJniEnv())->NewByteArray(length);
-        (*JniUtils::getJniEnv())->SetByteArrayRegion(byetArray, 0, length, buffer);
-        return byetArray;
+    static inline jshortArray getShortArrayFromBuffer(JNIEnv* env, short* buffer, int length) {
+        jshortArray shortArray = env->NewShortArray(length);
+        env->SetShortArrayRegion(shortArray, 0, length, buffer);
+        return shortArray;
     }
 
-    static inline jcharArray getCharArrayFromBuffer(jchar* buffer, int length) {
-        jcharArray charArray = (*JniUtils::getJniEnv())->NewCharArray(length);
-        (*JniUtils::getJniEnv())->SetCharArrayRegion(charArray, 0, length, buffer);
+    static inline jbyteArray getByteArrayFromBuffer(JNIEnv* env, signed char* buffer, int length) {
+        jbyteArray byteArray = env->NewByteArray(length);
+        env->SetByteArrayRegion(byteArray, 0, length, buffer);
+        return byteArray;
+    }
+
+    static inline jcharArray getCharArrayFromBuffer(JNIEnv* env, jchar* buffer, int length) {
+        jcharArray charArray = env->NewCharArray(length);
+        env->SetCharArrayRegion(charArray, 0, length, buffer);
         return charArray;
     }
 
-    static inline jbooleanArray getBooleanArrayFromBuffer(jboolean* buffer, int length) {
-        jbooleanArray booleanArray = (*JniUtils::getJniEnv())->NewBooleanArray(length);
-        (*JniUtils::getJniEnv())->SetBooleanArrayRegion(booleanArray, 0, length, buffer);
+    static inline jbooleanArray getBooleanArrayFromBuffer(JNIEnv* env, jboolean* buffer, int length) {
+        jbooleanArray booleanArray = env->NewBooleanArray(length);
+        env->SetBooleanArrayRegion(booleanArray, 0, length, buffer);
         return booleanArray;
-    }
-
-    static inline const cc_t* getReadConfiguration(jobject* object) {
-          jint fd = JniUtils::getPortDescriptorFromSerialPort(object);
-          const cc_t* readConfigBuffer = TerminalDevice::getReadConfigurationMode(&fd);
-          return readConfigBuffer;
     }
 }
 
