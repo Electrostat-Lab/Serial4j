0b592de2bf8d0614a6ee845e73fee8457a49c7ad
pavl_g <scrappers.tm@gmail.com>
2023-10-05 08:10:15 +0300

    com_serial4j_core_terminal_NativeFilePermissions: implemented POSIX NativeFilePermissions port to Java code

---
+++ serial4j-native/src/lib/jni/com_serial4j_core_terminal_NativeFilePermissions.cpp
@@ -0,0 +1,27 @@
+#include<jni/com_serial4j_core_terminal_NativeFilePermissions.h>
+#include<fcntl.h>
+
+JNIEXPORT jint JNICALL Java_com_serial4j_core_terminal_NativeFilePermissions_getReadOnly
+  (JNIEnv* env, jclass clazz) {
+    return O_RDONLY;
+}
+
+JNIEXPORT jint JNICALL Java_com_serial4j_core_terminal_NativeFilePermissions_getWriteOnly
+  (JNIEnv* env, jclass clazz) {
+    return O_WRONLY;
+}
+
+JNIEXPORT jint JNICALL Java_com_serial4j_core_terminal_NativeFilePermissions_getReadWrite
+  (JNIEnv* env, jclass clazz) {
+    return O_RDWR;
+}
+
+JNIEXPORT jint JNICALL Java_com_serial4j_core_terminal_NativeFilePermissions_getNoControlTerminalDevice
+  (JNIEnv* env, jclass clazz) {
+    return O_NOCTTY;
+}
+
+JNIEXPORT jint JNICALL Java_com_serial4j_core_terminal_NativeFilePermissions_getTerminalNonBlock
+  (JNIEnv* env, jclass clazz) {
+    return O_NONBLOCK;
+}
