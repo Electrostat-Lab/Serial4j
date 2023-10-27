76ad2b04d4a2a48ecc8fc37d3a6b72bf11c3a084
pavl_g <scrappers.tm@gmail.com>
2023-10-28 00:12:58 +0300

    HID: added an initial HID and serial-based HID API

---
+++ serial4j/src/main/java/com/serial4j/core/hid/SerialInterfaceDevice.java
@@ -0,0 +1,147 @@
+/*
+ * BSD 3-Clause License
+ *
+ * Copyright (c) 2022, Scrappers Team, The AVR-Sandbox Project
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package com.serial4j.core.hid;
+
+import com.serial4j.core.serial.SerialPort;
+import com.serial4j.core.terminal.FilePermissions;
+import com.serial4j.core.terminal.ReadConfiguration;
+import com.serial4j.core.terminal.TerminalDevice;
+
+/**
+ * Provides a standard implementation for a serial-based HID.
+ *
+ * <p>
+ * Any serial-based HID is associated with a {@link TerminalDevice}
+ * instance that opens the port, initializes terminal IO, and handles
+ * raw read/write operations, after which comes the decoder algorithms
+ * which is left as an open-ended API for the serial-based devices, wherein
+ * each serial-based HID should define its standard report descriptor through these
+ * algorithms, see {@link com.serial4j.core.hid.shiftavr.JoystickDevice} as an example.
+ * </p>
+ *
+ * @param <T> the type-arg of the decoded data packets
+ * @author pavl_g
+ */
+public class SerialInterfaceDevice<T> extends HumanInterfaceDevice<T> {
+
+    /**
+     * The associated terminal device instance for handling terminal
+     * operations.
+     */
+    protected TerminalDevice terminalDevice;
+
+    /**
+     * The serial port this device is connected to.
+     */
+    protected SerialPort serialPort;
+
+    /**
+     * The file permissions for this serial port filesystem operations.
+     */
+    protected FilePermissions operativePermissions;
+
+    /**
+     * Instantiates a serial-based human-interface-device (HID) with a terminal device to handle
+     * terminal IO and line-speed operations, and a serial port to which this device is connected to.
+     *
+     * @param terminalDevice the associated terminal device
+     * @param serialPort     the serial port to which this device is connected to
+     */
+    public SerialInterfaceDevice(final TerminalDevice terminalDevice, final SerialPort serialPort) {
+        this.terminalDevice = terminalDevice;
+        this.serialPort = serialPort;
+    }
+
+    @Override
+    public void init() {
+        terminalDevice.setOperativeFilePermissions(operativePermissions);
+        terminalDevice.openPort(serialPort);
+        terminalDevice.initTerminal();
+        // POLLING requested bytes out of the IO pipeline queue
+        // this makes the read() returns immediately in case
+        // of there are no bytes are in the terminal input queue
+        getTerminalDevice().setReadConfigurationMode(
+                ReadConfiguration.POLLING_READ,
+                0,
+                0
+        );
+    }
+
+    @Override
+    public void terminate() {
+        terminalDevice.closePort();
+        terminalDevice = null;
+        serialPort = null;
+        operativePermissions = null;
+        super.terminate();
+    }
+
+    @Override
+    public void decode() {
+        // assigns a read function and the raw packet of the input queue
+        super.decode(
+                integer -> terminalDevice.sread(reportDescriptor.getEncodedData().length()),
+                terminalDevice.getReadBuffer()
+        );
+    }
+
+    @Override
+    public void encode() {
+        // assigns a write function and the packet decoded from the output queue
+        super.encode(
+                raw -> terminalDevice.write(raw),
+                reportDescriptor.getDecodedData()
+        );
+    }
+
+    /**
+     * Retrieves a reference to the terminal device used
+     * for the read/write and terminal line-speed operations.
+     *
+     * @return a reference to the terminal device operations
+     */
+    public TerminalDevice getTerminalDevice() {
+        return terminalDevice;
+    }
+
+    /**
+     * Adjusts the file permissions for this application process (open/read/write operations)
+     * invoked by the terminal device on the filesystem associated with
+     * this device.
+     *
+     * @param operativePermissions the file permissions for this process open/read/write operations
+     */
+    public void setOperativePermissions(FilePermissions operativePermissions) {
+        this.operativePermissions = operativePermissions;
+    }
+}
--- serial4j/src/main/java/com/serial4j/core/hid/package-info.java
+++ serial4j/src/main/java/com/serial4j/core/hid/package-info.java
@@ -1,7 +1,7 @@
 /*
  * BSD 3-Clause License
  *
- * Copyright (c) 2022, Scrappers Team, The AVR-Sandbox Project, Serial4j API.
+ * Copyright (c) 2022, Scrappers Team, The AVR-Sandbox Project
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -30,49 +30,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package com.serial4j.example.jme;
-
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 /**
- * Decodes a UART frame into {@link JoystickValue}s.
- * 
- * @author pavl_g.
+ * Provides an abstract human-interface-device API.
  */
-public final class JoystickValueDecoder {
-
-    private JoystickValueDecoder() {
-    }
-
-    public static JoystickValue getJoystickValue(String frame) {
-        int x = 0;
-        int y = 0;
-        try {
-            for (int i = 0; i < frame.length(); i++) {
-                if (frame.charAt(i) == 'x') {
-                    // i + 4 is the delimiter between the x and the value of x
-                    x = getPotentiometerValue(frame, i + 4, ',');
-                } else if (frame.charAt(i) == 'y') {
-                    // i + 4 is the delimiter between the 'y' and the value of y
-                    y = getPotentiometerValue(frame, i + 4, ']');
-                }
-            }
-        } catch (Exception e) {
-            x = 0;
-            y = 0;
-            Logger.getLogger(JoystickValue.class.getName())
-                  .log(Level.WARNING, "Decoding Fails with '" + e.getMessage() + "'", e.getCause());
-        }
-
-        return new JoystickValue(x, y);
-    }
-
-    private static int getPotentiometerValue(String frame, int index, char delimiter) {
-        final StringBuilder data = new StringBuilder();
-        for (int i = index; frame.charAt(i) != delimiter; i++) {
-            data.append(frame.charAt(i));
-        }
-        return Integer.getInteger(data.toString(), 0);
-    }
-}
+package com.serial4j.core.hid;
