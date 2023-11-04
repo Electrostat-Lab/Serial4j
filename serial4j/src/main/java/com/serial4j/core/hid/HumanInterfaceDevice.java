2a5cdf71890e6c25fea2f7b8c346328f22b19892
pavl_g <scrappers.tm@gmail.com>
2023-11-04 21:02:55 +0300

    HumanInterfaceDevice: standardized the API and added JavaDoc

--- serial4j/src/main/java/com/serial4j/core/hid/HumanInterfaceDevice.java
+++ serial4j/src/main/java/com/serial4j/core/hid/HumanInterfaceDevice.java
@@ -32,6 +32,7 @@
 
 package com.serial4j.core.hid;
 
+import com.serial4j.core.hid.device.dataframe.DataFrameDevice;
 import java.util.function.Function;
 
 /**
@@ -48,17 +49,18 @@ import java.util.function.Function;
  * <a href="https://docs.kernel.org/hid">Linux-HID</a>
  * </p>
  *
- * @param <T> a type-arg for the decoded (concrete) post-processed data
+ * @param <D> a type-arg for the decoded (concrete) post-processed data
  * @author pavl_g
- * @see com.serial4j.core.hid.shiftavr.JoystickDevice Demo Joystick Serial-HID
+ * @see DataFrameDevice Demo Joystick Serial-HID
  */
-public abstract class HumanInterfaceDevice<T> {
+@SuppressWarnings("all")
+public abstract class HumanInterfaceDevice<E, D> {
 
     /**
      * An associated report descriptor that provides descriptive
      * data for IO packets.
      */
-    protected ReportDescriptor<T> reportDescriptor;
+    protected ReportDescriptor reportDescriptor;
 
     /**
      * An associated listener to be dispatched as a part of the
@@ -66,7 +68,7 @@ public abstract class HumanInterfaceDevice<T> {
      * the associated decoder {@link HumanInterfaceDevice#decoder},
      * typically utilized as a user-code.
      */
-    protected ReportDescriptor.DecoderListener<T> decoderListener;
+    protected ReportDescriptor.DecoderListener<E, D> decoderListener;
 
     /**
      * An associated decoder that provides the interface for the
@@ -75,7 +77,7 @@ public abstract class HumanInterfaceDevice<T> {
      * of the {@link HumanInterfaceDevice#decoderListener}, typically
      * utilized as a part of the defined HID standards (API code).
      */
-    protected ReportDescriptor.Decoder<T> decoder;
+    protected ReportDescriptor.Decoder<E, D> decoder;
 
     /**
      * Not directly instantiable class, inheritance
@@ -89,7 +91,7 @@ public abstract class HumanInterfaceDevice<T> {
      *
      * @return the reference to the device-associated report descriptor
      */
-    public ReportDescriptor<T> getReportDescriptor() {
+    public ReportDescriptor getReportDescriptor() {
         return reportDescriptor;
     }
 
@@ -99,7 +101,7 @@ public abstract class HumanInterfaceDevice<T> {
      *
      * @param reportDescriptor a report descriptor object
      */
-    public void setReportDescriptor(ReportDescriptor<T> reportDescriptor) {
+    public void setReportDescriptor(ReportDescriptor reportDescriptor) {
         this.reportDescriptor = reportDescriptor;
     }
 
@@ -110,7 +112,7 @@ public abstract class HumanInterfaceDevice<T> {
      *
      * @return a reference to the decoder listener component
      */
-    public ReportDescriptor.DecoderListener<T> getDecoderListener() {
+    public ReportDescriptor.DecoderListener<E, D> getDecoderListener() {
         return decoderListener;
     }
 
@@ -121,7 +123,7 @@ public abstract class HumanInterfaceDevice<T> {
      *
      * @param decoderListener a reference to the decoder listener
      */
-    public void setDecoderListener(ReportDescriptor.DecoderListener<T> decoderListener) {
+    public void setDecoderListener(ReportDescriptor.DecoderListener<E, D> decoderListener) {
         this.decoderListener = decoderListener;
     }
 
@@ -133,7 +135,7 @@ public abstract class HumanInterfaceDevice<T> {
      *
      * @return a reference to the report descriptor decoder instance
      */
-    public ReportDescriptor.Decoder<T> getDecoder() {
+    public ReportDescriptor.Decoder<E, D> getDecoder() {
         return decoder;
     }
 
@@ -142,7 +144,7 @@ public abstract class HumanInterfaceDevice<T> {
      *
      * @param decoder the new decoder instance used for encoding/decoding data
      */
-    protected void setDecoder(ReportDescriptor.Decoder<T> decoder) {
+    protected void setDecoder(ReportDescriptor.Decoder<E, D> decoder) {
         this.decoder = decoder;
     }
 
@@ -154,7 +156,7 @@ public abstract class HumanInterfaceDevice<T> {
      * feature reports, initializing line-speeds, and more.
      * </p>
      */
-    protected abstract void init();
+    public abstract void init();
 
     /**
      * Nullifies references for GC operations.
@@ -162,42 +164,52 @@ public abstract class HumanInterfaceDevice<T> {
      * Override to provide stream/IO/memory release operations.
      * </p>
      */
-    protected void terminate() {
+    public void terminate() {
         reportDescriptor = null;
         decoderListener = null;
     }
 
     /**
-     * Override to provide a decoding algorithm,
-     * usually a call to {@link HumanInterfaceDevice#decode(Function, String)} is made
+     * Override to provide receiving and decoding algorithms,
+     * usually a call to {@link HumanInterfaceDevice#decode(Function)} is made
      * with different parameter sets according to the device standards.
      */
-    protected abstract void decode();
+    public abstract void receive();
 
     /**
-     * Override to provide an encoding algorithm,
+     * Override to provide encoding and transmitting algorithms,
      * usually a call to {@link HumanInterfaceDevice#encode(Function, Object)} is made
      * with different parameter sets according to the device standards.
+     *
+     * @param decoded the decoded report to encode and send
      */
-    protected abstract void encode();
+    public abstract void transmit(final D decoded);
+
+    /**
+     * Device vendor data.
+     *
+     * @return the device vendors in string format
+     */
+    public abstract String getVendor();
 
     /**
      * Defines a default algorithm for executing decoding post-read operations.
      *
      * <p>
      * A call to this function dispatches the function
-     * {@link com.serial4j.core.hid.ReportDescriptor.DecoderListener#onDecodingCompleted(Object)}
+     * {@link ReportDescriptor.DecoderListener#onDecodingCompleted(Object)}
      * as a part of post-processing user operations.
      * </p>
      *
      * @param read a reference to the device read() function to dispatch
-     * @param raw  the raw data as an output of the reading operation to
-     *             be processed by the defined decoder
      */
-    protected final void decode(final Function<Integer, Long> read, final String raw) {
-        reportDescriptor.setEncodedData(raw);
-        read.apply(reportDescriptor.getEncodedData().length());
-        final T concreteData = decoder.decode(raw);
+    protected final void decode(final Function<Integer, E> read) {
+        // read report with data register buffer length
+        final E encoded = read.apply(reportDescriptor.getDataRegisterBufferLength());
+        if (encoded == null) {
+            return;
+        }
+        final D concreteData = decoder.decode(encoded);
         if (decoderListener != null) {
             decoderListener.onDecodingCompleted(concreteData);
         }
@@ -209,20 +221,107 @@ public abstract class HumanInterfaceDevice<T> {
      *
      * <p>
      * A call to this function dispatches the function
-     * {@link com.serial4j.core.hid.ReportDescriptor.DecoderListener#onEncodingCompleted(String)}
+     * {@link ReportDescriptor.DecoderListener#onEncodingCompleted(Object)}
      * as a part of post-processing user operations.
      * </p>
      *
      * @param write    a reference to the device write() function to dispatch
-     * @param concrete a decoded (concrete) data packet to be encoded before
+     * @param decoded a decoded (concrete) data packet to be encoded before
      *                 being sent to the device
      */
-    protected final void encode(final Function<String, Long> write, final T concrete) {
-        reportDescriptor.setDecodedData(concrete);
-        final String raw = decoder.encode(concrete);
-        write.apply(raw);
-        if (decoderListener != null) {
-            decoderListener.onEncodingCompleted(raw);
+    protected final long encode(final Function<E, Long> write, final D decoded) {
+        final E encoded = decoder.encode(decoded);
+        if (encoded == null) {
+            return 0;
+        }
+        final long bytes = write.apply(encoded);
+        if (decoderListener != null && bytes > 0) {
+            decoderListener.onEncodingCompleted(encoded);
+        }
+        return bytes;
+    }
+
+    /**
+     * Defines the simplest form of the
+     * report type with a length attribute in bytes units.
+     */
+    protected interface ReportDescriptor {
+
+        /**
+         * Defines the total report length in "bytes".
+         *
+         * <p>
+         * Example: 1 for 1 byte (char), 4 for 4 bytes (int).
+         * </p>
+         *
+         * @return report length in bytes
+         */
+        int getReportLength();
+
+        /**
+         * Defines the size of the data register buffer
+         * required for reading operations in "bytes".
+         *
+         * <p>
+         * For 8-bit microcontrollers, this should be 1-byte (aka. 8-bits).
+         * Unlike the report length, this attribute defines the data length
+         * per frame (per single clock transmission).
+         * </p>
+         *
+         * @return the input buffer length in bytes
+         */
+        int getDataRegisterBufferLength();
+
+        /**
+         * Defines a standard to decode/encode data
+         * packets for this report descriptor.
+         *
+         * @param <D> the type of the decoded data packets
+         */
+        interface Decoder<E, D> {
+
+            /**
+             * Encodes concrete data packets before sending
+             * to the device.
+             *
+             * @param decoded the data data packet to encode.
+             * @return an encoded version of the data packet ready to send
+             * to the I/O pipeline
+             */
+            E encode(D decoded);
+
+            /**
+             * Decodes received raw data packets into a concrete version.
+             *
+             * @param encoded the raw received data packets
+             * @return a decoded version of the received data packets
+             */
+            D decode(E encoded);
+        }
+
+        /**
+         * Defines a dispatch listener that is dispatched
+         * when decoding/encoding completes, usually this
+         * is a user-side API.
+         *
+         * @param <D> the type of the decoded data packets
+         */
+        interface DecoderListener<E, D> {
+
+            /**
+             * Dispatched when encoding a data packet completes
+             * before sending it to the device.
+             *
+             * @param encoded the raw data packets
+             */
+            void onEncodingCompleted(E encoded);
+
+            /**
+             * Dispatched when decoding a received data packet completes.
+             *
+             * @param decoded the decoded data packets
+             */
+            void onDecodingCompleted(D decoded);
         }
     }
 }
